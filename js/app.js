edb9b99646b7e3fba46c34e3598d0741caf8f251
Yuriy Yurinskiy <yuriyyurinskiy@yandex.ru>
2022-02-06 21:37:27 +0700

    build: очищает прошлый проект

--- js/app.js
+++
@@ -1,6 +0,0 @@
-let toggle = document.querySelector('.toggle');
-let navigation = document.querySelector('.navigation');
-toggle.onclick = function (e) {
-    e.currentTarget.classList.toggle('active');
-    navigation.classList.toggle('active');
-}
