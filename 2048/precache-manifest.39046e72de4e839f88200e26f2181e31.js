ca340c0128ef82df570cd0887306cb5aa2ddc92b
Yuriy Yurinskiy <yuriyyurinskiy@yandex.ru>
2022-02-06 21:38:48 +0700

    feat: добавляет тестовый билд 2048

---
+++ 2048/precache-manifest.39046e72de4e839f88200e26f2181e31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab01e37af0bca4e54845",
+    "url": "/css/app.f9cd8b2b.css"
+  },
+  {
+    "revision": "91e161a764f3648b144678d44b108ec7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab01e37af0bca4e54845",
+    "url": "/js/app.03a4c666.js"
+  },
+  {
+    "revision": "d25cac45c4dfa82e903b",
+    "url": "/js/chunk-vendors.a5e01114.js"
+  },
+  {
+    "revision": "999cb864e10ddc58ca55e116f0219960",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
